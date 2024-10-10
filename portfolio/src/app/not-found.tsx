2e6b57a8ff6d6be4429739b07b66d7979d6ec40e
Sarah Colin <srh.colin@gmail.com>
2024-10-10 11:50:41 +0200

    404 page

--- portfolio/src/app/not-found.tsx
+++ portfolio/src/app/not-found.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function NotFound() {
   return (
     <div>
         <h2>Page non trouvée</h2>
+        <Link href="/" className="text-2xl font-bold tracking-wide">
+            Retourner sur le portfolio
+          </Link>
+        
     </div>
   )
 }
